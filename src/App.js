5330790cc8ff9e2134289ca3c587750d8b54c7c1
Michael Hutchinson <michaelhutchinson@michaels-mbp.mynet>
2020-05-12 20:11:30 +0100

    Introduce props and data to create components
    - Added data for my todo list in an array
    - Added props functionality
    - Mapped my data to items to display in the components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import TodoItem from './components/TodoItem';
+import todoData from './todoData'
+
 import './assets/css/style.css';
 
 function App() {
+  const todoItems = todoData.map(item => <TodoItem key={item.id} item={item} />)
   return (
     <div className="todo-list">
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
+      {todoItems}
     </div>
   );
 }
---
+++ src/todoData.js
@@ -0,0 +1,29 @@
+const todoData = [
+    {
+        id: 1,
+        text: "Learn React 1",
+        completed: true
+    },
+    {
+        id: 2,
+        text: "Learn React 2",
+        completed: false
+    },
+    {
+        id: 3,
+        text: "Learn React 3",
+        completed: false
+    },
+    {
+        id: 4,
+        text: "Learn React 4",
+        completed: true
+    },
+    {
+        id: 5,
+        text: "Learn React 5",
+        completed: false
+    }
+]
+
+export default todoData
